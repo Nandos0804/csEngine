0c5f0ae69cce0a501dc5cc304f06afff57ffed5e
Giuseppe Ernandez <giuseppeernandez@hotmail.it>
2026-07-04 20:25:16 +0200

    feat: improve jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,13 @@
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: ["public/**/*.js"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
